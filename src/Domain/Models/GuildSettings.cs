7e570d37432d1a21f87baf1a256584705f569a62
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2026-02-19 12:31:48 +0100

    mark welcome message as sent to prevent double sends (#11)

--- src/Domain/Models/GuildSettings.cs
+++ src/Domain/Models/GuildSettings.cs
@@ -6,6 +6,7 @@ public class GuildSettings : BaseEntity
     public required string GuildName { get; set; } // Discord server name (may become outdated)
     public ulong? StatsChannelId { get; set; } // Where to post weekly stats
     public bool IsEnabled { get; set; } = true;
+    public bool HasSentWelcomeMessage { get; set; } = false;
     public DayOfWeek WeeklySummaryDay { get; set; } = DayOfWeek.Sunday;
     public TimeSpan WeeklySummaryTime { get; set; } = new TimeSpan(18, 0, 0); // 6 PM
 
